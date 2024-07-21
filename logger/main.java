0eb32727d5d01707770a92b0f304de4415aba545
Achyuth_kumar_k <44013200+achyuth-kumar@users.noreply.github.com>
2024-07-21 18:26:24 +0530

    Update main.java

--- logger/main.java
+++ logger/main.java
@@ -1,1 +1,9 @@
+public class Main {
+   public static void main(String args[]) {
+        LogProcessor logProcessor=new InfoProcessor(new DebugProcessor(new ErrorProcessor(null)));
+        logProcessor.log(logProcessor.INFO,"print requested information");
+        logProcessor.log(logProcessor.DEBUG,"print debugger logs");
+        logProcessor.log(logProcessor.ERROR,"print error logs");
+    } 
+}
 
